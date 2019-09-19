fac34e3ea9aea0547fc026d96d92d0d5926865e1
Carmen <sdluojm@gmail.com>
2019-09-19 20:02:00 +0000

    refactor with database with Suggested Project Structure

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,13 +1,4 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightdb'
-});
+const db = require('../db/index');
 /// Users
 
 /**
@@ -22,7 +13,7 @@ const getUserWithEmail = function (email) {
   from users
   where email = $1;
   `
-  return pool.query(queryString, [email])
+  return db.query(queryString, [email])
     .then(res => {
       if (res.rows.length > 0) {
         return (res.rows[0])
@@ -43,7 +34,7 @@ const getUserWithId = function (id) {
   from users
   where id = $1;
   `
-  return pool.query(queryString, [id])
+  return db.query(queryString, [id])
     .then(res => {
       if (res.rows.length > 0) {
         return (res.rows[0])
@@ -62,7 +53,7 @@ exports.getUserWithId = getUserWithId;
 const addUser = function (user) {
   const values = [user.name, user.email, user.password]
   const queryString = `INSERT INTO users(name, email, password) VALUES ($1, $2, $3) RETURNING *`
-  return pool.query(queryString, values)
+  return db.query(queryString, values)
     .then(res => res.rows[0])
     .catch(err => console.log('query error', err.stack))
 }
@@ -81,7 +72,7 @@ const getAllReservations = function (guest_id, limit = 10) {
   // from users join reservations on user.id = reservations.guest_id
   // where guest_id = $1 AND reservations.end_date < now() :: date;
   // `
-  // pool.query(queryString,[guest_id])
+  // db.query(queryString,[guest_id])
   // .then(res => {
   //   res.rows
   // })
@@ -95,7 +86,7 @@ GROUP BY reservations.id, properties.id, property_reviews.id
 ORDER BY reservations.start_date
 LIMIT $2`;
   const values = [guest_id, limit];
-  pool.query(queryString, values)
+ db.query(queryString, values)
     .then(res => (res.rows))
     .catch(err => console.log('query error', err.stack))
 
@@ -151,7 +142,7 @@ const getAllProperties = function (options, limit = 10) {
   LIMIT $${queryParams.length};`
   console.log(queryString, queryParams);
 
-  return pool.query(queryString, queryParams)
+  return db.query(queryString, queryParams)
     .then(res => res.rows);
 }
 exports.getAllProperties = getAllProperties;
@@ -182,7 +173,7 @@ const values = [
 const queryString = 
 `INSERT INTO properties (owner_id,title, description,thumbnail_photo_url, cover_photo_url, cost_per_night,street,city, province,post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`
-return pool.query(queryString, values)
+return db.query(queryString, values)
 .then(res => res.rows)
 .catch(err => console.log('query error', err.stack))
 }
